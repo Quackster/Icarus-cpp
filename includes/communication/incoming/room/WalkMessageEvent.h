d0d414ed40e1beefdb073d931f2aa22316048794
Alex <imploderific@gmail.com>
2016-10-12 05:32:47 +1000

    WalkMessageEvent cleanup

--- includes/communication/incoming/room/WalkMessageEvent.h
+++ includes/communication/incoming/room/WalkMessageEvent.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "communication/incoming/MessageEvent.h"
 #include "game/pathfinder/Pathfinder.h"
 
@@ -12,19 +13,11 @@ public:
         int goal_x = request.readInt();
         int goal_y = request.readInt();
 
-        printf(" walk req: %i, %i\n", goal_x, goal_y);
-
-        Position current = Position(player->getRoomUser()->getX(), player->getRoomUser()->getY());
-        Position goal = Position(goal_x, goal_y);
         Room *room = player->getRoomUser()->getRoom();
 
-        std::vector<Position> paths = Pathfinder::makePath(current, goal, room);
+        RoomUser *room_user = player->getRoomUser();
+        room_user->setGoalX(goal_x);
+        room_user->setGoalY(goal_y);
 
-        std::cout << "size: " << paths.size() << std::endl;
-
-        for (auto path : paths) {
-            std::cout << path.toString() << std::endl;
-        }
-
-    }
+        std::vector<Position> paths = Pathfinder::makePath(room_user->getPosition(), room_user->getGoal(), room);
 };
