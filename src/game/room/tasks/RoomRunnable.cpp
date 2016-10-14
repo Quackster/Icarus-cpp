1db89fb5e90ed872290d6a1198d7156802e8a4fb
Alex <imploderific@gmail.com>
2016-10-14 12:42:38 +1000

    Fix compile error

--- src/game/room/tasks/RoomRunnable.cpp
+++ src/game/room/tasks/RoomRunnable.cpp
@@ -24,10 +24,50 @@ int Calculate(int X1, int Y1, int X2, int Y2);
 */
 RoomRunnable::RoomRunnable(Room *room) : room(room) { }
 
-/*
-    Tick handler for room runnable
+int Calculate(int X1, int Y1, int X2, int Y2)
+{
+    int Rotation = 0;
 
-    @return none
+    if (X1 > X2 && Y1 > Y2)
+    {
+        Rotation = 7;
+    }
+    else if (X1 < X2 && Y1 < Y2)
+    {
+        Rotation = 3;
+    }
+    else if (X1 > X2 && Y1 < Y2)
+    {
+        Rotation = 5;
+    }
+    else if (X1 < X2 && Y1 > Y2)
+    {
+        Rotation = 1;
+    }
+    else if (X1 > X2)
+    {
+        Rotation = 6;
+    }
+    else if (X1 < X2)
+    {
+        Rotation = 2;
+    }
+    else if (Y1 < Y2)
+    {
+        Rotation = 4;
+    }
+    else if (Y1 > Y2)
+    {
+        Rotation = 0;
+    }
+
+    return Rotation;
+}
+
+/*
+Tick handler for room runnable
+
+@return none
 */
 void RoomRunnable::run() {
 
@@ -42,7 +82,7 @@ void RoomRunnable::run() {
     for (Entity *entity : *this->room->getEntities()) {
 
         RoomUser *room_user = entity->getRoomUser();
-        
+
 
         if (room_user->getPosition().sameAs(room_user->getGoal())) {
             room_user->stopWalking(false);
@@ -61,7 +101,7 @@ void RoomRunnable::run() {
 
                 int height = room_model->getSquareHeight()[next.getX() * room_model->getMapSizeY() + next.getY()];
                 room_user->setRotation(Calculate(room_user->getPosition().getX(), room_user->getPosition().getY(), next.getX(), next.getY()), true, false);
-                
+
                 std::stringstream ss;
                 ss << next.getX();
                 ss << ",";
@@ -91,6 +131,6 @@ void RoomRunnable::run() {
     if (entities_update.size() > 0) {
         this->room->send(UserStatusMessageComposer(entities_update));
     }
-    
+
     Icarus::getGame()->getGameScheduler()->schedule(this->room->getRunnable());
 }
