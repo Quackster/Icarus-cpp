a8e0d488aee2167d49ca90b9f5c8f50870d7840f
Alex <imploderific@gmail.com>
2016-10-16 22:42:27 +1000

    Fixed room score

--- includes/game/room/model/RoomModel.h
+++ includes/game/room/model/RoomModel.h
@@ -23,9 +23,9 @@ public:
     std::string getHeightMap() { return height_map; }
     std::string getFloorMap() { return floor_map; }
 
-    std::string *&getSquareChar() { return square_char; }
-    double *&getSquareHeight() { return square_height; }
-    int* getSquares() { return squares; }
+    std::string *getSquareChar() { return square_char; }
+    double *getSquareHeight() { return square_height; }
+    int *getSquares() { return squares; }
     bool isValidSquare(int x, int y) { return squares[x * map_size_y + y] == 0; }
 
     int &getMapSizeX() { return this->map_size_x; }
